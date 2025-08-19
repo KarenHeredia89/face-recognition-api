7d2d895db43595fdeaf1ead8cc634b5232e111d9
KarenHeredia89 <karenherediadg@gmail.com>
2025-08-18 20:18:41 -0600

    Redis config

--- src/config/redisClient.ts
+++ src/config/redisClient.ts
@@ -1,7 +1,13 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL,
+  // url: process.env.REDIS_URL_DEV,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_URL,
+    port: parseInt(process.env.REDIS_PORT || "6379", 10),
+  },
 });
 
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
